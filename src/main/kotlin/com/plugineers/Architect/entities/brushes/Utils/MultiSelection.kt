bbfe7c9e791e44aecef07ca219387ca11b14e6fc
Aztec <Rezel@users.noreply.github.com>
2018-08-17 18:19:11 -0600

    Update MultiSelection.kt
    Deprecated class (read notes)

--- src/main/kotlin/com/plugineers/Architect/entities/brushes/Utils/MultiSelection.kt
+++ src/main/kotlin/com/plugineers/Architect/entities/brushes/Utils/MultiSelection.kt
@@ -9,7 +9,10 @@ import org.bukkit.util.Vector
 
 /**
  * Select multiple edit regions or uneditable regions.
+ * @DevNote this is being removed, instead of just selecting uneditable region player can create a selected region and create a layer and then
+ set it to uneditable.  Also if this layer is the current selected layer for the player then it will  be the only one they can change.
  */
+@Deprecated
 class MultiSelection : CoreBrush() {
     override fun onCommand(player: Player, args: MutableList<String>) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
